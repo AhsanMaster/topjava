755987f382d11716a1aecdc399d8ddfee10f55df
Alexander Masson <mr.masson@mail.ru>
2019-10-14 22:10:07 +0700

    tasks 4

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,23 +1,24 @@
 package ru.javawebinar.topjava.service;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
+import ru.javawebinar.topjava.web.meal.MealRestController;
 
 import java.util.Collection;
 
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
-
+@Service
 public class MealService {
 
-    private MealRepository repository;
+    private  MealRepository repository;
 
-    // @Autowired
-    public MealService(MealRepository repository) {
+    @Autowired
+    public MealService(MealRepository repository){
         this.repository = repository;
     }
 
